4c1912bb54f612001864d5c8bb1fb2c867d4d5ac
Elliott <91552870+Bowriing@users.noreply.github.com>
2024-01-25 19:26:51 +0000

    Some UI Tweaks also added feature for deleting uploaded CSV file which also removes the CSV associated with the userID from SQL database

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/')
 @login_required
 def home():
     #retrieve the csv data if any from user
