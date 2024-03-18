80cfb1c7145f47875bd146aa17d13ce78052a895
Elliott <91552870+Bowriing@users.noreply.github.com>
2024-03-18 09:19:19 +0000

    some fixes

--- website/processing.py
+++ website/processing.py
@@ -1,4 +1,5 @@
 from flask_login import current_user
+from flask import flash
 
 class CsvLog:
     def __init__(self, pkwh, pdateTime):
@@ -6,24 +7,27 @@ class CsvLog:
         self.datetime = pdateTime
 
 def processingMain():
-    logInstance = getCsvLog()
-    print ("Datetime: " + logInstance.datetime)
-    print ("kWh Usage: " + logInstance.kwh + "kWh")
-    wattUsage = kwhToWatts(logInstance.kwh)
-    print (wattUsage)
+    if hasattr(current_user.csv_data, 'csv_content'):
+        logInstance = getCsvLog()
+        print ("Datetime: " + logInstance.datetime)
+        print ("kWh Usage: " + logInstance.kwh + "kWh")
+        wattUsage = kwhToWatts(logInstance.kwh)
+        print (wattUsage)
+        print ("Successfully outputted wattUsage")
+
+    elif not hasattr(current_user.csv_data, 'csv_content'):
+        flash('No CSV Content to read.', category='error')
+        print ("User's CSV Content Is Empty")
 
 
 
 def getCsvLog():
     csvData = current_user.csv_data.csv_content
     csvRow = [] 
-
     for row in csvData.split('\n')[1:]:
         csvRow.append(row)  
-
     csvItem = csvRow[0]
     csvItemSplit = csvItem.split(',')
-
     newLog = CsvLog(csvItemSplit[1], csvItemSplit[2])
 
     return newLog
